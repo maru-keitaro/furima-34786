5d97b50bdc610d78e36d72073d0de0f44f3212fb
maru-keitaro <maru.keitaro@gmail.com>
2021-03-18 15:13:30 +0900

    バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,20 @@
 class Item < ApplicationRecord
-  
-  belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :delivery_day_id
+    validates :price
+    validates :image
+  end
+
+  validates :genre_id, numericality: { other_than: 1 } 
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+  has_one_attached :image
 end
