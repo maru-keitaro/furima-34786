32bd96da21b94001f9bff247eea06b9d488e6c34
maru-keitaro <maru.keitaro@gmail.com>
2021-03-18 17:39:28 +0900

    商品出品ページを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
+    @items = Item.order("created_at DESC")
   end
+
+  def new
+    @item = Item.new
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+
+
 end
