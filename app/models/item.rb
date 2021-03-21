9eb0c8e1f4a617da859aef451487a0e10f034794
maru-keitaro <maru.keitaro@gmail.com>
2021-03-21 18:41:56 +0900

    修正2

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :category
   belongs_to :condition
   belongs_to :delivery_fee
@@ -15,12 +16,14 @@ class Item < ApplicationRecord
   end
 
   validates :price, numericality:{ only_integer:true,greater_than_or_equal_to:300,less_than_or_equal_to:9999999 }
-  validates :category_id, numericality: { other_than: 0 } 
-  validates :condition_id, numericality: { other_than: 0 } 
-  validates :delivery_fee_id, numericality: { other_than: 0 } 
-  validates :prefecture_id, numericality: { other_than: 0 } 
-  validates :delivery_day_id, numericality: { other_than: 0 } 
 
+  with_options numericality: { other_than: 0 }
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :prefecture_id} 
+    validates :delivery_day_id
+  end
   
   belongs_to :user
   has_one_attached :image
